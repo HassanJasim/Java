c989e4b13b81a88e31132f651ae2c99aca059d04
Hassan Jasim <hassanjasim1000@gmail.com>
2019-02-04 16:12:53 -0500

    A Collection of small programs
    Program 1
    - Creates a deck of cards and shuffles them
    Program 2
    - Coin toss program
    Program 3
    - Transposes a matrix

---
+++ 2XB3_Lab2/src/cas/lab2/wt/Transpose.java
@@ -0,0 +1,44 @@
+package cas.lab2.wt;
+
+public class Transpose {
+	public static void main (String [] args) {
+		// create n by n matrix
+		int n = Integer.parseInt(args[0]);
+		int [][] a = new int [n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				a[i][j] = n*i+j;
+			}
+		}
+		// print initial matrix
+		System.out.println("Before");
+		System.out.println("--------");
+		for (int i =0; i<n;i++) {
+			for (int j =0; j<n; j++) {
+				System.out.printf("%4d", a[i][j]);
+			}
+			System.out.println();
+		}
+		// transpose 
+		for (int i = 0; i< n; i++) {
+			for (int j = i+1; j <n; j++) {
+				int temp = a[i][j];
+				a[i][j] = a[j][i];
+				a[j][i]=temp;
+			}
+				
+		}
+		// print out transposed matrix
+		System.out.println();
+		System.out.println("After");
+		System.out.println("------");
+		for (int i=0; i <n; i++ ) {
+			for (int j =0; j < n; j++) {
+				System.out.printf("%4d", a[i][j]);
+			}
+			System.out.println();
+		}
+			
+	}
+
+}
